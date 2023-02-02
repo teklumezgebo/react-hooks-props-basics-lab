260c9083335f7ad663704ea1ce80c637b10fabbe
teklumezgebo <mezge002@umn.edu>
2023-02-01 21:29:55 -0600

    commit tree 170dad7d30bc86733891999c894476c8839444b9

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import user from "../data/user";
+import Link from "./Links"
+
+
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {<Link />}
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+import user from "../data/user";
+
+function Link() {
+    return (
+        <div>
+            <h3>Links</h3>
+            <a href={links}>https://github.com/liza</a>
+            <a href={links}>https://www.linkedin.com/in/liza/</a>
+        </div>
+    )
+}
+
+export default Link 
